d8cfba2ee66bafe9fbfd97a91bcaeced500b7ddc
Sabin Subedi <sabinsubedi125@gmail.com>
2021-05-10 15:54:17 +0545

    Simple server and frontend setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
 
 const App = () => {
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <Router>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Route path='/' component={HomeScreen} exact />
+          <Route path='/product/:id' component={ProductScreen} exact />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   )
 }
 
---
+++ frontend/src/products.js
@@ -0,0 +1,85 @@
+const products = [
+  {
+    _id: '1',
+    name: 'Airpods Wireless Bluetooth Headphones',
+    image: '/images/airpods.jpg',
+    description:
+      'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers ommersive listening experience Built-in microphone allows you to take calls while working',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 89.99,
+    countInStock: 3,
+    rating: 4.5,
+    numReviews: 4,
+  },
+  {
+    _id: '2',
+    name: 'Iphone 11 Pro 256 Memory',
+    image: '/images/phone.jpg',
+    description:
+      'Introducing the Iphone 11 pro. A transformation trple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life.',
+
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 599.99,
+    countInStock: 30,
+    rating: 4.0,
+    numReviews: 3,
+  },
+  {
+    _id: '3',
+    name: 'Canon E05 80D DSLR Camera',
+    image: '/images/camera.jpg',
+    description:
+      'Characterized by verstile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design',
+
+    brand: 'Canon',
+    category: 'Electronics',
+    price: 859.99,
+    countInStock: 5,
+    rating: 5,
+    numReviews: 3,
+  },
+  {
+    _id: '4',
+    name: 'Sony Playstation 4 Pro White Version',
+    image: '/images/playstation.jpg',
+    description:
+      'The ultimate home entertainment center starts with Playstation. Whether you are into gaming, HD movies, television ,music',
+
+    brand: 'Sony',
+    category: 'Electronics',
+    price: 399.99,
+    countInStock: 10,
+    rating: 2,
+    numReviews: 3,
+  },
+  {
+    _id: '5',
+    name: 'Logitech G-Series Gaming Mouse',
+    image: '/images/mouse.jpg',
+    description:
+      'Get better handle on your games with this logitech LIGHTSYNC gaming mouse. The six programmable butoons allow customization for a smooth playing experience',
+    brand: 'Logitech',
+    category: 'Electronics',
+    price: 29.99,
+    countInStock: 20,
+    rating: 3.5,
+    numReviews: 3,
+  },
+  {
+    _id: '6',
+    name: 'Amazon Echo DOT AI Voice Assistant',
+    image: '/images/alexa.jpg',
+    description:
+      'Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space',
+    brand: 'Amazon',
+    category: 'Electronics',
+    price: 59.99,
+    countInStock: 0,
+    rating: 3.5,
+    numReviews: 4,
+  },
+]
+
+export default products
